1f87e274c9c503edb2b67a45153fe974baed748a
Asa-Chen <ac581218@ohio.edu>
2020-09-03 16:33:00 -0400

    Adding git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Asa Chen>
+ *        Date: <September 03, 2020>
+ * Description: Example program for Lab 02
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
